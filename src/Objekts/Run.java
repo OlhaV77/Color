88d06ada8a9625fb6a651166b752e63a96f739d5
olgaV77 <olga.v1977@gmail.com>
2021-02-21 11:45:06 -0500

    print object

--- src/Objekts/Run.java
+++ src/Objekts/Run.java
@@ -14,8 +14,26 @@ class Run {
     Green - 0 255 0
  */
 
+/*
+
+        for (int i = 0; i < 5; i++) {
+
+            colors[i].name =  new String[]{"Red", "Blue", "Green", "White", "Yellow"};
+            colors[i].redColorField =  new int[]{255, 0, 0, 255, 255};
+            colors[i].blueColorField = new int[]{0, 255, 0, 255, 0};
+            colors[i].greenColorField = new int[]{0, 0, 128, 255, 255};
+
+            colors[i] = new Color(colors[i].name[i], colors[i].redColorField[i], colors[i].blueColorField[i], colors[i].greenColorField[i]);
+
+
+
+
+ */
+
+
     public static void main(String[] args) {
         Color[] colors = new Color[5];
+
         colors[0] = new Color("Red", 255, 0, 0);
         colors[1] = new Color("Blue", 0, 255, 0);
         colors[2] = new Color("Green", 0, 0, 128);
@@ -23,9 +41,8 @@ class Run {
         colors[4] = new Color("Yellow", 255, 0, 255);
 
 
-
-        for (int i = 0; i < 5; i++) {
-            System.out.println(colors[i].name + ": " + colors[i].redColorField  + " " + colors[i].greenColorField + " " + colors[i].blueColorField);
+        for (int i = 0; i < colors.length; i++) {
+            System.out.println(colors[i]);
         }
     }
 }
@@ -41,10 +58,12 @@ class Color {
         this.redColorField = redColorField;
         this.greenColorField = greenColorField;
         this.blueColorField = blueColorField;
+    }
 
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", redColorField=" + redColorField +
+                ", greenColorField=" + greenColorField +
+                ", blueColorField=" + blueColorField;
     }
 }
-
-
-
-
